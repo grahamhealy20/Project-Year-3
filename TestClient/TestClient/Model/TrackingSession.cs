ef30141d843cef1f5a55e1603b540a2f9a8eb9ca
Graham Healy <grahamh220@gmail.com>
2015-03-18 00:43:14 +0000

    test

---
+++ TestClient/TestClient/Model/TrackingSession.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClient.Model
+{
+    class TrackingSession
+    {
+        private Kinect sensor;
+        public int startTracking() {
+            try {
+                // Start Kinect
+                sensor = new Kinect();
+
+                // Start Temp
+                return 1;    
+
+            } catch(Exception ex) {
+                return 0;
+            }
+        }
+      
+
+
+
+
+    }
+}
