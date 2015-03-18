b6dac58301303ea867dbeed5bd679c72267412ef
Graham Healy <grahamh220@gmail.com>
2015-03-18 12:39:44 +0000

    Temp scaffold

---
+++ TestClient/TestClient/Model/Sensor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClient.Model
+{
+  class Sensor
+  {
+// Add Tracking Code here
+
+    public void start() { 
+      // Open conn to serial port
+    }
+
+    public void getTemp() { 
+      //get resistance from 
+    //convert to temp    
+    }
+  }
+}
