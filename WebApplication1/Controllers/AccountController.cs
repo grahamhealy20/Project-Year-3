2a361bf542ceb6b345e63fd629bd363c9b3080a4
Graham Healy <grahamh220@gmail.com>
2015-04-13 19:29:31 +0100

    Profile images implemented

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -337,10 +337,20 @@ namespace WebApplication1.Controllers
             if (file != null && file.ContentLength > 0)
                 try
                 {
-                    string path = Path.Combine(Server.MapPath("~/Images"),
-                                               Path.GetFileName(file.FileName));
+                    var fileName = Path.GetFileName(file.FileName);
+                    var dbPath = "~/App_Data/uploads/" + fileName; 
+                    var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
                     file.SaveAs(path);
-                    ViewBag.Message = "File uploaded successfully";
+
+
+                    string userId = User.Identity.GetUserId();
+                    var store = new UserStore<ApplicationUser>(new ApplicationDbContext());
+                    var manager = new UserManager<ApplicationUser>(store);
+                    var user = manager.FindById(userId);
+                    user.profileImagePath = dbPath;
+                    manager.UpdateAsync(user);
+                    var ctx = store.Context;
+                    ctx.SaveChanges();
                 }
                 catch (Exception ex)
                 {
