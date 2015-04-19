372974eddae284336b5f15f896a40cfe3b0bdbe5
Graham Healy <grahamh220@gmail.com>
2015-04-19 14:41:49 +0100

    Line Chart Implemented

--- WebApplication1/Controllers/TrackingController.cs
+++ WebApplication1/Controllers/TrackingController.cs
@@ -21,27 +21,55 @@ namespace WebApplication1.Controllers
         // GET: Tracking
         public ActionResult Index()
         {
-          try {
-              objects = GetAllTrackingStateByID(User.Identity.GetUserId());
-              Success(string.Format("List successfully retrieved at: <strong>{0}</strong>", DateTime.Now.ToString()), true);
-           }
-          catch (Exception ex) { 
-            Danger(string.Format("<strong>ERROR: </strong>{0}", ex.Message), true);
-           }    
+            try
+            {
+                objects = GetAllTrackingStateByID(User.Identity.GetUserId());
+                Success(string.Format("List successfully retrieved at: <strong>{0}</strong>", DateTime.Now.ToString()), true);
+            }
+            catch (Exception ex)
+            {
+                Danger(string.Format("<strong>ERROR: </strong>{0}", ex.Message), true);
+            }
             // objects.Add(new Models.TrackingModel.TrackingState(2, "3:00", "Ballyfermot", 30.00, 4));
             return View(objects);
         }
 
-        public ActionResult Sessions() {
+        public ActionResult Sessions()
+        {
             return View();
         }
         public ActionResult Session(int id)
         {
             Models.TrackingModel.Session session;
-            using (var db = new Models.ApplicationDbContext()) {
-                 session = db.Sessions.Include(x => x.states).Single(x => x.Id == id);
+            using (var db = new Models.ApplicationDbContext())
+            {
+                session = db.Sessions.Include(x => x.states).Single(x => x.Id == id);
             }
             return View(session);
+        }
+
+        public ActionResult GetDataForPlot(int id)
+        {
+            Models.TrackingModel.Session session;
+            using (var db = new Models.ApplicationDbContext())
+            {
+                session = db.Sessions.Include(x => x.states).Single(x => x.Id == id);
+            }
+            var data = new List<Models.TrackingModel.TrackingTempLineChartModel>();
+            //    new Models.TrackingModel.TrackingTempLineChartModel { date ="1", temp = "30"}
+            //};
+            foreach (var state in session.states)
+            {
+                if (state.time != null) {
+                    data.Add(new Models.TrackingModel.TrackingTempLineChartModel(state.time, state.temp.ToString()));
+                }
+                
+            }
+            JsonResult jr = new JsonResult();
+            jr.Data = data;
+            jr.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return jr;
+            //return Json(data, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult ListOfStatesID()
@@ -61,19 +89,21 @@ namespace WebApplication1.Controllers
         }
 
 
-        public ActionResult TrackingInfoID() {
+        public ActionResult TrackingInfoID()
+        {
             //state = GetTrackingState(Convert.ToInt32(User.Identity.GetUserId()));
-            
+
             state = GetTrackingState(2147483647);
             return View(state);
         }
 
-        public List<Models.TrackingModel.TrackingState> GetAllTrackingState() { 
+        public List<Models.TrackingModel.TrackingState> GetAllTrackingState()
+        {
             byte[] toByte = proxy.DownloadData((new Uri("http://localhost:4082/TrackingService.svc/GetAllTrackingState")));
 
-            Stream strm = new MemoryStream(toByte); 
+            Stream strm = new MemoryStream(toByte);
             DataContractSerializer obj = new DataContractSerializer(typeof(List<Models.TrackingModel.TrackingState>));
-            var objects = (List<Models.TrackingModel.TrackingState>) obj.ReadObject(strm);
+            var objects = (List<Models.TrackingModel.TrackingState>)obj.ReadObject(strm);
             return objects;
         }
 
@@ -89,7 +119,7 @@ namespace WebApplication1.Controllers
         public List<Models.TrackingModel.TrackingState> GetAllTrackingStateByID(string id)
         {
             string user_id = id;
-        //a99f2883-8ed3-4583-afdb-3f570f159cf3
+            //a99f2883-8ed3-4583-afdb-3f570f159cf3
             byte[] toByte = proxy.DownloadData((new Uri("http://localhost:4082/TrackingService.svc/TrackingState/" + id)));
 
             Stream strm = new MemoryStream(toByte);
@@ -98,7 +128,8 @@ namespace WebApplication1.Controllers
             return objects;
         }
 
-        public string Welcome() {
+        public string Welcome()
+        {
             return "This is the welcome action method.";
         }
     }
---
+++ WebApplication1/Models/TrackingModel/TrackingViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.TrackingModel
+{
+    public class TrackingViewModel
+    {
+    }
+
+    [Serializable]
+    public class TrackingTempLineChartModel {
+        public string date {get; set;}
+        public string temp {get; set;}
+
+        public TrackingTempLineChartModel() { }
+        public TrackingTempLineChartModel(string time, string temp) {
+            this.date = time;
+            this.temp = temp;
+        }
+    }
+}
