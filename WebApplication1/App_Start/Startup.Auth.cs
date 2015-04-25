55214495d934b96a5c871d92441132bc0e8dd054
Graham Healy <grahamh220@gmail.com>
2015-04-25 12:26:27 +0100

    Revert "Testing IIS"
    This reverts commit d571ae4cbbb50c64f8b34aab293a3917f6a0e6d7.

--- WebApplication1/App_Start/Startup.Auth.cs
+++ WebApplication1/App_Start/Startup.Auth.cs
@@ -34,7 +34,7 @@ namespace WebApplication1
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
             });
-
+            
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
             // Uncomment the following lines to enable logging in with third party login providers
