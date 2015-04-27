e19b95d09433a982e973570ecbd85f6a276b88a6
Graham Healy <grahamh220@gmail.com>
2015-04-27 12:27:46 +0100

    Fixed CORS

--- TrackingRESTService/TrackingRESTService/Startup.cs
+++ TrackingRESTService/TrackingRESTService/Startup.cs
@@ -3,14 +3,16 @@ using Microsoft.Owin;
 using Owin;
 using TrackingRESTService;
 using Microsoft.AspNet.SignalR;
-
+using Microsoft.Owin.Cors;
 namespace TrackingRESTService
 {
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseCors(CorsOptions.AllowAll);
             app.MapSignalR();
+
         }
     }
 }
